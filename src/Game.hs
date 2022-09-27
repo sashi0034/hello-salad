f9c652d795f55abae53bf2d903400fbd616be04f
sashi0034 <satoshi3104.nishi@gmail.com>
2022-09-27 14:20:04 +0900

    キー入力で動かす方法を学んだ

--- src/Game.hs
+++ src/Game.hs
@@ -1,19 +1,17 @@
 -- 参考: https://qiita.com/lotz/items/eb73e62a64bc208c2dd6 
 
 module Game where
-
 import Graphics.Gloss
-import Graphics.Gloss.Data.ViewPort
+import Graphics.Gloss.Interface.IO.Game
 
 -------------------
 -- Display の設定
 -------------------
 
 windowWidth, windowHeight :: Num a => a
-windowWidth  = 640
+windowWidth = 640
 windowHeight = 480
 
--- Display in a window with the given name, size and position.
 window :: Display
 window = InWindow "Hello World" (windowWidth, windowHeight) (100, 100)
 
@@ -32,33 +30,49 @@ data BoxState = BoxState
   , _vy :: Float -- y 方向の速度
   }
 
+
 initialBox :: BoxState
-initialBox = BoxState 0 0 150 150
+initialBox = BoxState 0 0 0 0
 
 drawBox :: BoxState -> Picture
 drawBox box = translate (_x box) (_y box) $ rectangleSolid boxWidth boxHeight
 
-nextBox :: ViewPort -> Float -> BoxState -> BoxState
-nextBox vp dt box =
+-- | イベントを処理する関数。EventKey以外のイベントは無視する
+updateBox :: Event -> BoxState -> BoxState
+updateBox (EventKey key ks _ _) box = updateBoxWithKey key ks box
+updateBox (EventMotion _)       box = box
+updateBox (EventResize _)       box = box
+
+-- | 上下左右の速度を与える関数
+up, down, right, left :: BoxState -> BoxState
+up    box = box { _vy = _vy box + 100 }
+down  box = box { _vy = _vy box - 100 }
+right box = box { _vx = _vx box + 100 }
+left  box = box { _vx = _vx box - 100 }
+
+-- | 方向キーとWASDキーに対応して四角形を移動させる
+updateBoxWithKey :: Key -> KeyState -> BoxState -> BoxState
+updateBoxWithKey (SpecialKey KeyUp)    ks = if ks == Down then up    else down
+updateBoxWithKey (SpecialKey KeyDown)  ks = if ks == Down then down  else up
+updateBoxWithKey (SpecialKey KeyRight) ks = if ks == Down then right else left
+updateBoxWithKey (SpecialKey KeyLeft)  ks = if ks == Down then left  else right
+updateBoxWithKey (Char 'w')            ks = if ks == Down then up    else down
+updateBoxWithKey (Char 's')            ks = if ks == Down then down  else up
+updateBoxWithKey (Char 'd')            ks = if ks == Down then right else left
+updateBoxWithKey (Char 'a')            ks = if ks == Down then left  else right
+updateBoxWithKey _ _ = id
+
+nextBox :: Float -> BoxState -> BoxState
+nextBox dt box =
   let -- 速度を考慮した次のステップでの位置を計算
       x  = _x box + _vx box * dt
       y  = _y box + _vy box * dt
 
-      -- 壁との当たり判定
-      isOverTop    = y >  (windowHeight - boxHeight) / 2
-      isOverBottom = y < -(windowHeight - boxHeight) / 2
-      isOverRight  = x >  (windowWidth - boxWidth) / 2
-      isOverLeft   = x < -(windowWidth - boxWidth) / 2
-
-      -- 壁と衝突していれば速度を反転させる
-      vx = if isOverRight || isOverLeft   then (-_vx box) else (_vx box)
-      vy = if isOverTop   || isOverBottom then (-_vy box) else (_vy box)
-
-   in BoxState x y vx vy
+   in box { _x = x, _y = y }
 
 -------------
 -- main 関数
 -------------
 
 main :: IO ()
-main = simulate window white 24 initialBox drawBox nextBox
+main = play window white 24 initialBox drawBox updateBox nextBox
